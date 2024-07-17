215f381c104f276ff6b98f7157bb16c345102333
rajaFlutter <raja.dey@iskylar.in>
2024-07-17 13:13:32 +0530

    Updated and Fixed Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,33 @@
+"use client"
 import React from "react";
-import bannerImg from "../../public/bannerImage.png";
 import Image from "next/image";
+import bannerImg from "../../public/bannerImage.png";
 
-export default function Homepage() {
-  // flex flex-row items-center justify-between
+
+const Homepage = () => {
+
+
   return (
     <main className="flex flex-col justify-center bg-nav-color items-center h-full">
-      <div className="w-full flex flex-col items-start justify-between gap-5 lg:gap-0 lg:flex-row lg:items-center px-5 lg:px-20 xl:px-44 3xl:px-60 4xl:px-72 mt-5 lg:mt-0">
+      <div className="w-full flex-col-start-between gap-5 lg:gap-0 lg:flex-row-center-between px-5 lg:px-20 xl:px-44 3xl:px-60 4xl:px-72 mt-5 lg:mt-0">
         <div className="w-full lg:w-[70%] flex flex-col gap-3">
           <div className="w-full text-[2.5rem] leading-[3rem] lg:leading-[4rem] text-black-main lg:text-[2vmax] xl:text-[2.5vmax] font-semibold">
-            <p>Empower Open Source with REPOS,</p>
-            <span className="text-color-primary">
-              A Sustainable Funding Model
-            </span>
+            <p>
+              Empower Open Source with REPOS,
+            </p>
+            <span className="text-color-primary">A Sustainable Funding Model</span>
           </div>
           <div className="w-full lg:w-[85%] text-[1.2rem] lg:text-xs xl:text-sm 3xl:text-lg">
             <span>
-              REPOS (Reimbursed Enhancement Proposals for Open Source) is a
-              platform that connects open source developers with sponsors and
-              contributors. Whether you want to support existing projects or
-              propose new ideas, REPOS provides a sustainable way to fund and
-              drive innovation in the open source ecosystem.
+              REPOS (Reimbursed Enhancement Proposals for Open Source) is a platform that connects open source developers with sponsors and contributors. Whether you want to support existing projects or propose new ideas, REPOS provides a sustainable way to fund and drive innovation in the open source ecosystem.
             </span>
           </div>
         </div>
         <div className="w-full h-[20rem] lg:w-[28%] lg:h-[20vmax] flex-row-center-end relative">
-          <Image
-            src={bannerImg}
-            alt="Banner Image"
-            fill={true}
-            className="w-full h-full object-contain"
-          />
+          <Image src={bannerImg} alt="Banner Image" fill={true} className="w-full h-full object-contain" />
         </div>
       </div>
-      <div className="w-full flex-col items-center justify-between gap-8 lg:gap-0 bg-white px-8 lg:px-20 xl:px-52 3xl:px-80 py-7">
+      <div className="w-full flex-col-center-between gap-8 lg:gap-0 bg-white px-8 lg:px-20 xl:px-52 3xl:px-80 py-7">
         <div className="lg:px-10 xl:px-0 lg:w-[80%] xl:w-1/2 flex-col-center-center text-black-main gap-5 lg:gap-2 3xl:gap-3">
           <h2 className="text-[2.5rem] leading-[3rem] lg:text-[2vmax] xl:text-[2.5vmax] font-semibold lg:text-center">
             Find Your Next Project
@@ -43,6 +37,10 @@ export default function Homepage() {
           </p>
         </div>
       </div>
+      <section className="w-full h-[40vh] py-10 xl:px-20 3xl:px-60">
+      </section>
     </main>
   );
-}
+};
+
+export default Homepage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,47 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'ss': '0px',
+      'sm': '598px',
+      'md': '766px',
+      'md1': '818px',
+      'md2': '910px',
+      'lg': '1022px',
+      'xl': '1278px',
+      '2xl': '1438px',
+      '3xl': '1534px',
+      '4xl': '1918px',
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +49,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animate")
+  ],
 };
 export default config;
