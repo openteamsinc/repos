814ec4250f4f55f93c8d001379afadd0a5d5e3e5
rajaFlutter <109644874+rajaFlutter@users.noreply.github.com>
2024-07-17 18:56:52 +0530

    Added Navbar and fixed hero section in homepage (#14)
    * Added Navbar
    
    * Updated and Fixed Hero section
    
    * updated files
    
    * updated files
    
    ---------
    
    Co-authored-by: Sean Ross-Ross <srossross@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +23,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
+        <Navbar />
         {children}
         <Footer />
       </body>
