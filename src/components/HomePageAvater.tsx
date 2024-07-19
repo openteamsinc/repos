acd72411c9a9d377e9ae3e2d8ad26b6102a31aaa
rajaFlutter <raja.dey@iskylar.in>
2024-07-19 14:10:32 +0530

    GitHub usernames with profile picture in pages

--- src/components/HomePageAvater.tsx
+++ src/components/HomePageAvater.tsx
@@ -1,14 +1,31 @@
+"use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/shadCDN/avatar"
 import {
     HoverCard,
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import { useEffect, useState } from "react"
 
 export const HomePageAvatar = ({ name, profileImage }: { name: string, profileImage: string }) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    // Open URL in a new window
+    const openNewWindow = (e: React.MouseEvent<HTMLAnchorElement>, url: string) => {
+        window.open(url, '_blank', '');
+    }
+
+    if (!isMounted) {
+        return <span>{name}</span>
+    }
+
     return <div>
         <HoverCard openDelay={0} closeDelay={0}>
-            <HoverCardTrigger>
+            <HoverCardTrigger onClick={(e) => openNewWindow(e, `https://github.com/${name}`)}>
                 <Avatar className="w-10 h-10">
                     <AvatarImage src={profileImage} />
                     <AvatarFallback>CN</AvatarFallback>
